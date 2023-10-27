e15cc23ebc3703505cd1af5dd18f1f950fa10920
rudradeep22 <rudradeep22@iitk.ac.in>
2023-10-27 23:31:52 +0530

    Added problem

---
+++ prob4.hs
@@ -0,0 +1,27 @@
+import System.IO
+main::IO()
+main = do
+    contents <- readFile "input.txt"
+    let result = length $ filter (==True) $ map check $ map extractNumbers $ formatter contents
+    print result
+
+formatter str = lines str
+
+splitNumbers :: String -> [String]
+splitNumbers [] = []
+splitNumbers (x:xs)
+    | x `elem` "-," = splitNumbers xs
+    | otherwise = let (num, rest) = span (`notElem` "-,") (x:xs)
+                  in num : splitNumbers rest
+
+extractNumbers :: String -> [Int]
+extractNumbers str = map read (splitNumbers str)
+
+check::[Int] -> Bool
+check (a:b:c:d:_)
+    | (a-c)*(b-d) <= 0 = True
+    | otherwise        = False
+
+check2::[Int] -> Bool
+check2 (a:b:c:d:_)
+    | 
